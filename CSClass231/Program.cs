7daa1ad18e1b32706e66c76ba9ac8b799d8858b8
Gihun Ham <ham@e-mirim.hs.kr>
2022-08-24 08:59:46 +0900

    #3 out 키워드 실습

--- CSClass231/Program.cs
+++ CSClass231/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,8 @@ namespace CSClass231
 {
     internal class Program
     {
+
+
         static void Main(string[] args)
         {
             Wanted<string> wantedString = new Wanted<string>("String");
@@ -20,6 +23,29 @@ namespace CSClass231
             Products p = new Products();
             Console.WriteLine(p[4]);
             p[4] = 5;
+
+            Console.WriteLine("숫자 입력: ");
+            int output;
+            bool result = int.TryParse(Console.ReadLine(), out output);
+            if (result)
+            {
+                Console.WriteLine("입력한 숫자: " + output);
+            }
+            else
+            {
+                Console.WriteLine("숫자를 입력해주세요!");
+            }
+            int x = 0; int y = 0;
+            int vx = 1; int vy = 1;
+            Console.WriteLine("현재 좌표: (" + x + "," + y + ")");
+            NextPos(x, y, vx, vy, out x, out y);
+            Console.WriteLine("다음 좌표: (" + x + "," + y + ")");
+
+        }
+        static void NextPos(int x, int y, int vx, int vy, out int rx, out int ry)
+        {
+            rx = x + vx;
+            ry = y + vy;
         }
     }
 }
